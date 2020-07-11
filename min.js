74e0b2c2a1d5277dedb5284ef7b9e014a2b46fd1
Muhammad Irfan Ali <aliirfan1337@gmail.com>
2020-07-11 10:39:22 +0500

    Delete min.js

--- min.js
+++
@@ -1,8 +0,0 @@
-console.log("Hey js working fine");
-function closeSideBar(){
-    document.getElementById("mynav").style.width="0px";
-    
-}
-function OpenSideBar(){
-    document.getElementById("mynav").style.width="250px";
-}
